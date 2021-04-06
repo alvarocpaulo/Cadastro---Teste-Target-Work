272cfcfd38397e6ca386c6488cf0c4a90aaf1870
Álvaro Costa Paulo <acptdb@gmail.com>
2021-04-06 18:39:26 -0300

    continue

--- Cadastro/Domain/Interfaces/IProductRepository.cs
+++ Cadastro/Domain/Interfaces/IProductRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Cadastro.Domain.Interfaces
 {
-    interface IProductRepository
+    public interface IProductRepository
     {
         Product Get(int id);
         IEnumerable<Product> GetAll();
